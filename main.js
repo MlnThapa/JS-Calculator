239c28352f829108b845db2ddaa8107a0e8da27b
Milan <milan@Milans-MacBook-Air.local>
2023-04-27 15:12:04 +0530

    Changes in css and js

--- main.js
+++ main.js
@@ -1,42 +1,30 @@
-let container = document.getElementById('container');
-let calculator = document.getElementById('calculator');
-let toggleImage = document.getElementById('toggleImage');
-let toggleButton = document.getElementById('toggle-button');
+let container = document.querySelector(".container");
+let calculator = document.querySelector(".calculator")
+let toggleImage = document.querySelector('.toggleImage');
+let toggleButton = document.querySelector('.toggle-button');
 let inputField = document.getElementsByTagName('input');
 let bool = true;
 let toggleLightDark = ()=>{
-    for(let i=0;i<inputField.length;i++){
-        console.log(inputField[i].classList.toggle('input-dark'))
+    for(let i=1;i<inputField.length;i++){
+        inputField[i].classList.toggle('input-dark')
     }
+    container.classList.toggle('dark')
+    calculator.classList.toggle('dark')
+    toggleImage.classList.toggle('dark')
+    toggleButton.classList.toggle('dark')
     if(bool === true){
-        container.classList.remove('container-light')
-        container.classList.add('container-dark')
-        calculator.classList.remove('calculator-light')
-        calculator.classList.add('calculator-dark')
-        toggleImage.classList.remove('toggleImageLight')
-        toggleImage.classList.add('toggleImageDark')
         toggleButton.value = "DARK"
-        toggleButton.classList.remove('toggle-button-light')
-        toggleButton.classList.add('toggle-button-dark')
         bool = false
-
     }else{
-        container.classList.remove('container-dark')
-        container.classList.add('container-light')
-        calculator.classList.add('calculator-light')
-        calculator.classList.remove('calculator-dark')
-        toggleImage.classList.add('toggleImageLight')
-        toggleImage.classList.remove('toggleImageDark')
         toggleButton.value = "LIGHT"
-        toggleButton.classList.remove('toggle-button-dark')
-        toggleButton.classList.add('toggle-button-light')
         bool = true;
     }
-    
 }
+
 let inputValue = (val) => {
     document.getElementById("display").value += val;
 }
+
 let del = ()=>{
     document.getElementById("display").value = document.getElementById("display").value.slice(0,-1);
 }
